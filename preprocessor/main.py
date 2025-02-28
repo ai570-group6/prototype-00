62f31b25307c52ef53275daf2b7b80ffd74d89c9
Chris Umbel <chris@chrisumbel.com>
2025-02-27 23:30:31 -0500

    started preprocessor

---
+++ preprocessor/main.py
@@ -0,0 +1,27 @@
+import os
+import glob
+import sys
+import numpy as np
+from PIL import Image
+
+def save_image(img_array, path):
+    img = Image.fromarray(img_array)
+    img.save(path)
+
+def preprocess_image(img_array):
+    print(img_array.shape)
+    img_array[img_array < 20] = 0
+    img_array[img_array > 120] = 255
+    return img_array
+
+def preprocess_files(root, target_path):
+    for path in glob.glob(root + '/*'):
+        print(path)
+        img = Image.open(path)
+        img_array = np.array(img)
+        img_array = preprocess_image(img_array)
+        file_name = os.path.basename(path)
+        save_image(img_array, os.path.join(target_path, file_name))
+
+preprocess_files(sys.argv[1], sys.argv[2])
+
